60ecf85022567e2af5530e9332034de7f254db20
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-29 21:08:53 -0300

    feat: Configurated svg in project and add logo.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from 'react-native'
 import { styles } from './styles'
 
+import Logo from '../../assets/logo.svg'
+
 export function Home() {
   return (
     <View style={styles.container}>
+      <View>
+        <Logo height={32} />
+      </View>
       <Text>Home</Text>
     </View>
   )
