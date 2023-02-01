5da2b8e535aebed8cb1e86aaf305b452e192d7f9
Vagner Nerves <vagnernervessantos@gmail.com>
2023-01-31 22:57:30 -0300

    feat: Ajusted Home.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+
 import { styles } from './styles'
 
-import Logo from '../../assets/logo.svg'
+import { Header } from '../../components/Header'
+import { NewTask } from '../../components/NewTask'
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <View>
-        <Logo height={32} />
-      </View>
-      <Text>Home</Text>
+      <Header />
+      <NewTask />
     </View>
   )
 }
