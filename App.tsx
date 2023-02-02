f82586e9dfd0ab1648abee30243d8c38cc1a60c6
Vagner Nerves <vagnernervessantos@gmail.com>
2023-02-01 21:34:08 -0300

    feat: Instaled font Inter Regular and Bold.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import { StatusBar } from 'react-native'
 import { Home } from './src/screens/Home'
 
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold
+} from '@expo-google-fonts/inter'
+import { Loading } from './src/components/Loading'
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_700Bold })
+
   return (
     <>
       <StatusBar
@@ -9,8 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-
-      <Home />
+      {fontsLoaded ? <Home /> : <Loading />}
     </>
   )
 }
